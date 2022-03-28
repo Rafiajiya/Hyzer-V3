4e63f4750dbaf51441525a9de491ea772e8e88cf
Rafiajiya <94803405+Rafiajiya@users.noreply.github.com>
2022-03-29 04:22:10 +0700

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,25 +3,25 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 ╭═════════════════
 ║╭──❉ *𝐁𝐈𝐎𝐃𝐀𝐓𝐀 𝐎𝐖𝐍𝐄𝐑* ❉───
-║│➸ *Nama* : Hyzer ( nama samaran ) 
-║│➸ *Umur* : 15
-║│➸ *Kelas* :  9 SMP
-║│➸ *Status* : can't be good atau everything
+║│➸ *Nama* : Rafi Aji
+║│➸ *Umur* : 17
+║│➸ *Kelas* :  2 SMK
+║│➸ *Status* : Penyuka Tt kecil yang imut🤫
 ║╰───────────────
 ╰════════════════
 ╭════════════════
 ║╭──❉ *𝐒𝐎𝐒𝐈𝐀𝐋 𝐌𝐄𝐃𝐈𝐀* ❉───
-║│➸ *No.Owner* : wa.me/6287892711054
-║│➸ *No.Bot* : wa.me/6287726311156
-║│➸ *Chanel Youtube* : https://youtube.com/channel/UC3Y7rHuI_OmbvFdL88JF7AQ
-║│➸ *Gmail* : Xnxx@gmail.com
-║│➸ *Github* : https://github.com/Hyzerr 
+║│➸ *No.Owner* : wa.me/6288233481992
+║│➸ *No.Bot* : wa.me/62882007331744
+║│➸ *Chanel Youtube* : Rafi X1
+║│➸ *Gmail* : Bokep@gmail.com
+║│➸ *Github* : https://github.com/Rafiajiya
 ║╰────────────────
 ╰═════════════════
-_Oke udah itu aja terimakasih_
+_Oke udah itu aja Ya Kontol_
 
 `.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Rafi')).buffer(), ext, 'Recoded By Rafi', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 
 }
 handler.help = ['infoowner', 'infocreator']
